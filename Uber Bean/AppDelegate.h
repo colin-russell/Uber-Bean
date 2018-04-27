e0ea9acf0457b56c391e4cc0a27eabdae86b6e77
Colin Russell <colinrussell101@gmail.com>
2018-04-27 14:26:36 -0400

    Initial Commit

---
+++ Uber Bean/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Uber Bean
+//
+//  Created by Colin on 2018-04-27.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Uber Bean/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Uber Bean
+//
+//  Created by Colin on 2018-04-27.
+//  Copyright © 2018 Colin Russell. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
